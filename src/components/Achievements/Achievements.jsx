af7d8c4e0e9f863a0e6f89389e2b6d26ca5304fa
Vishal Pratap Singh <vishal19111999@hotmail.com>
2021-05-21 15:29:53 +0530

    feat: added achievements section

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -9,6 +9,24 @@ export default function MoreAbout() {
           Achievements <span alt={"trophy emoji"}>🏆</span>
         </b>
       </div>
+      <b>
+        <div className={"neu-achievements"} style={{ marginTop: "18vh" }}>
+          CodeChef 6 star ⭐️ coder
+        </div>
+        <div className={"neu-achievements"} style={{ marginTop: "6vh" }}>
+          Won CodeChef DSA certification 📜 (100%) scholarship in Feb Long
+          Challenge
+        </div>
+        <div className={"neu-achievements"} style={{ marginTop: "6vh" }}>
+          Global Rank 59 🏆 in September 2020 long challenge (Div 1)
+        </div>
+        <div className={"neu-achievements"} style={{ marginTop: "6vh" }}>
+          AIR 172 in Google HashCode 2020 🎉
+        </div>
+        <div className={"neu-achievements"} style={{ marginTop: "6vh" }}>
+          Problem setter ✅ at various contests
+        </div>
+      </b>
     </div>
   )
 }
